67988e1aca268a61a08c6cab3b6785857b318f3e
b_a_x <ily-odinokov@yandex.ru>
2020-07-10 21:49:49 +0300

    Переименовал проект

--- src/Desktop.Win/MainPage.xaml.cs
+++ src/Desktop.Win/MainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
