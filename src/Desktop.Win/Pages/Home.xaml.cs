c28e647ea3c82b7a87c9e2d7458abb34f90cefc8
b_a_x <ily-odinokov@yandex.ru>
2020-07-20 12:08:32 +0300

    ListView

--- src/Desktop.Win/Pages/Home.xaml.cs
+++ src/Desktop.Win/Pages/Home.xaml.cs
@@ -10,36 +10,7 @@ namespace Desktop.Win.Pages
         public Home()
         {
             this.InitializeComponent();
-            List<Lesson> lessons = new List<Lesson>
-            {
-                new Lesson
-                {
-                    Title = "asdasd",
-                    Author = "asdasd",
-                    Number = 2
-                },
-                new Lesson
-                {
-                    Title = "adfasf",
-                    Author = "dgvsdvbs",
-                    Number = 12
-                },
-                new Lesson
-                {
-                    Title = "fjfhkjf",
-                    Author = "vzbfdhgkyui",
-                    Number = 15
-                },
-                new Lesson
-                {
-                    Title = "fjfhkjf",
-                    Author = "vzbfdhgkyui",
-                    Number = 20
-                }
-            };
-
-            LessonVM lessonVm = new LessonVM(lessons);
-            this.DataContext = lessonVm;
+            HomePageListView.ItemsSource = PasswordInfoVM.GetPasswordInfo();
         }
     }
 }
---
+++ src/Desktop.Win/ViewModel/PasswordInfoVM .cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Desktop.Win.ViewModel
+{
+    public class PasswordInfoVM
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public static ObservableCollection<PasswordInfoVM> GetPasswordInfo()
+        {
+            return new ObservableCollection<PasswordInfoVM>
+            {
+                new PasswordInfoVM {Name = "asdasd", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "ksdfksmd", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "afmadsmfcasd", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "kdmafkma", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "masdfkladfk", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "lds,m;flme", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = ";ldmf;lqmew", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = ",dmflkwme", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = "amsfkmqwk", Login = "asdasdasd", Password = "asdasdasd"},
+                new PasswordInfoVM {Name = " dmlkfewmm", Login = "asdasdasd", Password = "asdasdasd"}
+            };
+        }
+    }
+}
