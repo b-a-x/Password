17f595c7e9049a2fead174e6e45930b3f18b3915
b_a_x <ily-odinokov@yandex.ru>
2020-08-05 22:07:58 +0300

    fix PasswordInfo

--- src/Desktop.Win/Model/PasswordInfo.cs
+++ src/Desktop.Win/Model/PasswordInfo.cs
@@ -2,10 +2,16 @@
 {
     public class PasswordInfo
     {
+        private string oldPassword;
         public int Id { get; set; }
         public string Name { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public string OldPassword { get; set; }
+
+        public string OldPassword
+        {
+            get => oldPassword ?? string.Empty;
+            set => oldPassword = value;
+        }
     }
 }
